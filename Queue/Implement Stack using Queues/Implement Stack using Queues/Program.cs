b34ed8e968d2efff86a842e98827047b1175c084
Huyvictory <vominhhuy008@gmail.com>
2025-01-06 11:00:09 +0700

    Update Implement Stacks using Queues solution #1

---
+++ Queue/Implement Stack using Queues/Implement Stack using Queues/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace Implement_Stack_using_Queues;
+
+class Program
+{
+    public static void TestCase1()
+    {
+        MyStack myStack = new MyStack();
+        myStack.Push(1);
+        myStack.Push(2);
+        Console.WriteLine(myStack.Top()); // return 2
+        Console.WriteLine(myStack.Pop()); // return 2
+        Console.WriteLine(myStack.Empty()); // return False
+    }
+
+    public static void TestCase2()
+    {
+        MyStack myStack = new MyStack();
+        myStack.Push(1);
+        Console.WriteLine(myStack.Pop()); // return 1
+        Console.WriteLine(myStack.Empty()); // return True
+    }
+
+    public static void TestCase3()
+    {
+        MyStack myStack = new MyStack();
+        myStack.Push(1);
+        myStack.Push(2);
+        Console.WriteLine(myStack.Top()); // return 2
+        Console.WriteLine(myStack.Pop()); // return 2
+        Console.WriteLine(myStack.Empty()); // return False
+    }
+
+    public static void TestCase4()
+    {
+        MyStack myStack = new MyStack();
+        myStack.Push(1);
+        myStack.Push(2);
+        myStack.Push(3);
+        Console.WriteLine(myStack.Pop()); // return 3
+        Console.WriteLine(myStack.Pop()); // return 2
+        Console.WriteLine(myStack.Pop()); // return 1
+        Console.WriteLine(myStack.Empty()); // return True
+    }
+
+    static void Main(string[] args)
+    {
+        // TestCase1();
+        // TestCase2();
+        // TestCase3();
+        TestCase4();
+    }
+}
