3c9a5dddee3cefa72d0bb757e6742801c34e206f
Huyvictory <vominhhuy008@gmail.com>
2025-01-07 13:47:23 +0700

    Update Reverse Linked List using recursion approach

---
+++ Recursion/Reverse Linked List/Reverse Linked List/ListNode.cs
@@ -0,0 +1,13 @@
+namespace Reverse_Linked_List;
+
+public class ListNode
+{
+    public int val;
+    public ListNode next;
+
+    public ListNode(int val, ListNode next = null)
+    {
+        this.val = val;
+        this.next = next;
+    }
+}
