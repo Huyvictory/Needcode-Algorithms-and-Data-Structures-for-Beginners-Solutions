8c9f292a3ba94dac43e860b09e429a6c49ad16f4
Huyvictory <vominhhuy008@gmail.com>
2025-01-11 12:47:19 +0700

    Update Insertion Sort solution

---
+++ Sorting/Insertion Sort/Insertion Sort States Retrieval/Insertion Sort States Retrieval/Pair.cs
@@ -0,0 +1,13 @@
+namespace Insertion_Sort_States_Retrieval;
+
+public class Pair
+{
+    public int Key;
+    public string Value;
+
+    public Pair(int key, string value)
+    {
+        Key = key;
+        Value = value;
+    }
+}
