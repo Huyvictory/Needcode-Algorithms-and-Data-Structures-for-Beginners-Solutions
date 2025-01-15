c194f8097c41504d2536a12ed9c184d54e0b9706
Huyvictory <vominhhuy008@gmail.com>
2025-01-15 16:34:33 +0700

    Update Quick Sort implementation solution 1

---
+++ Sorting/Quick Sort/Quick Sort Implementation/Quick Sort Implementation/Pair.cs
@@ -0,0 +1,13 @@
+namespace Quick_Sort_Implementation;
+
+public class Pair
+{
+    public int Key;
+    public string Value;
+
+    public Pair(int key, string value)
+    {
+        Key = key;
+        Value = value;
+    }
+}
