a81c9d60fc9027b8a26b8295a4c974909325b297
Huyvictory <vominhhuy008@gmail.com>
2025-01-20 10:46:52 +0700

    Update Guess Number Higher or Lower solution

---
+++ Search Range/Guess Number Higher or Lower/Guess Number Higher or Lower/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace Guess_Number_Higher_or_Lower;
+
+class Program
+{
+    private static int pickedNumber = 6;
+
+    public static int guess(int num)
+    {
+        if (num > pickedNumber)
+        {
+            return -1;
+        }
+        else if (num < pickedNumber)
+        {
+            return 1;
+        }
+        else
+        {
+            return 0;
+        }
+    }
+
+    public static int GuessNumber(int n)
+    {
+        int left = 1;
+
+        while (left <= n)
+        {
+            int midPointer = left + ((n - left) / 2);
+
+            if (guess(midPointer) == -1)
+            {
+                n = midPointer - 1;
+            }
+            else if (guess(midPointer) == 1)
+            {
+                left = midPointer + 1;
+            }
+            else
+            {
+                return midPointer;
+            }
+        }
+
+        return -1;
+    }
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine(GuessNumber(10));
+    }
+}
