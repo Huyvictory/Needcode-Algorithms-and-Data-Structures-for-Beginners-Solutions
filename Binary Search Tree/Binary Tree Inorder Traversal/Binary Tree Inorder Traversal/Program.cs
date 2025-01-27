0b072d7336373f4e90988c45494adc354b947b15
Huyvictory <vominhhuy008@gmail.com>
2025-01-27 18:35:08 +0700

    Update Binary Tree Inorder Traversal solution 2

--- Binary Search Tree/Binary Tree Inorder Traversal/Binary Tree Inorder Traversal/Program.cs
+++ Binary Search Tree/Binary Tree Inorder Traversal/Binary Tree Inorder Traversal/Program.cs
@@ -13,6 +13,15 @@ class Program
         return rootNode;
     }
 
+    public static TreeNode TestCase2() {
+        TreeNode Node_4 = new TreeNode(4);
+        TreeNode Node_2 = new TreeNode(2, null, Node_4);
+        TreeNode Node_1 = new TreeNode(1, Node_2);
+       TreeNode rootNode = new TreeNode(3, Node_1);
+
+       return rootNode;
+    }
+
     public static IList<int> InorderTraversal(TreeNode root)
     {
         if (root == null)
@@ -25,9 +34,35 @@ class Program
         return result;
     }
 
+    
+
+    public static IList<int> InorderTraversalIteratively(TreeNode root) {
+        if (root == null)
+            return [];
+        
+        Stack<TreeNode> stack = new Stack<TreeNode>();
+        TreeNode cur = root;
+
+        while (cur != null || stack.Count > 0) {
+            while (cur != null) {
+                stack.Push(cur);
+                cur = cur.left;
+            }
+
+            var poppedTopStack = stack.Pop();
+            result.Add(poppedTopStack.val);
+            if (poppedTopStack.right != null) {
+                cur = poppedTopStack.right;
+            }
+        }
+        
+        return result;
+    }
+
     static void Main(string[] args)
     {
         var inorderTraversedList = InorderTraversal(TestCase1());
+        var inorderTraversedList2 = InorderTraversalIteratively(TestCase2());
 
         return;
     }
