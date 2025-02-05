51ab74f0c462447a1a00f2ec9233a757d0a14d06
Huyvictory <vominhhuy008@gmail.com>
2025-02-05 17:30:35 +0700

    Update Binary Tree Right Side View solution

---
+++ Binary Search Tree/Binary Tree Right Side View/Binary Tree Right Side View/Program.cs
@@ -0,0 +1,68 @@
+﻿namespace Binary_Tree_Right_Side_View;
+
+class Program
+{
+    public static TreeNode TestCase1() {
+        TreeNode Node_5 = new TreeNode(5);
+        TreeNode Node_4 = new TreeNode(4);
+
+        TreeNode Node_2 = new TreeNode(2, null, Node_5);
+        TreeNode Node_3 = new TreeNode(3, null, Node_4);
+
+        TreeNode rootNode = new TreeNode(1, Node_2, Node_3);
+
+        return rootNode;
+    }
+
+    public static TreeNode TestCase2() {
+        TreeNode Node_5 = new TreeNode(5);
+        TreeNode Node_4 = new TreeNode(4, Node_5);
+
+        TreeNode Node_2 = new TreeNode(2, Node_4);
+        TreeNode Node_3 = new TreeNode(3, null, Node_4);
+
+        TreeNode rootNode = new TreeNode(1, Node_2, Node_3);
+
+        return rootNode;
+    }
+
+    public static IList<int> RightSideView(TreeNode root)
+    {
+        if (root == null)
+            return [];
+
+        Queue<TreeNode> queue = new Queue<TreeNode>([root]);
+        
+        IList<int> result = [];
+
+        while(queue.Count > 0) {
+
+            int NodesLevel = queue.Count;
+            
+            for (int i = 1; i <= NodesLevel; i++) {
+                var traversedNode = queue.Dequeue();
+
+                if (traversedNode.left != null) {
+                    queue.Enqueue(traversedNode.left);
+                }
+
+                if (traversedNode.right != null) {
+                    queue.Enqueue(traversedNode.right);
+                }
+
+                if (i == NodesLevel) {
+                    result.Add(traversedNode.val);
+                }
+            }
+        }
+
+        return result;
+    }
+
+    static void Main(string[] args)
+    {
+        var rightSideViewResultList = RightSideView(TestCase2());
+
+        return;
+    }
+}
