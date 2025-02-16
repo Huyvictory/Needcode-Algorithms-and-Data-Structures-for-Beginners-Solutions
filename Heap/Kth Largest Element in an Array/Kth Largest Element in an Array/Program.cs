e7b9c60a4c4b2374342545256b95dcd08e49906a
Huyvictory <vominhhuy008@gmail.com>
2025-02-16 22:20:30 +0700

    Update Kth Largest Element in an Array solution

---
+++ Heap/Kth Largest Element in an Array/Kth Largest Element in an Array/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace Kth_Largest_Element_in_an_Array;
+
+class Program
+{
+    private static (int[] nums, int k) TestCase1() {
+        return ([3,2,1,5,6,4], 2);
+    }
+
+    private static (int[] nums, int k) TestCase2() {
+        return ([3,2,3,1,2,4,5,5,6], 4);
+    }
+
+    private static PriorityQueue<int, int> HeapifyMax(int[] nums)
+    {
+        PriorityQueue<int, int> heapMax = new PriorityQueue<int, int>();
+
+        foreach (int num in nums)
+        {
+            heapMax.Enqueue(num, num > 0 ? -num : Math.Abs(num));
+        }
+
+        return heapMax;
+    }
+
+    public static int FindKthLargest(int[] nums, int k)
+    {
+        var heapMax = HeapifyMax(nums);
+
+        while (k > 1)
+        {
+            heapMax.Dequeue();
+            k--;
+        }
+
+        return heapMax.Peek();
+    }
+
+    static void Main(string[] args)
+    {
+        // Console.WriteLine(FindKthLargest(TestCase1().nums, TestCase1().k));
+        Console.WriteLine(FindKthLargest(TestCase2().nums, TestCase2().k));
+    }
+}
