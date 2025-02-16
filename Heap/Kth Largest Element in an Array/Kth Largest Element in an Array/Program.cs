52928756db86ca5d3fd4552a4d09fb2da75d23bf
Huyvictory <vominhhuy008@gmail.com>
2025-02-17 00:22:57 +0700

    Kth Largest Element in an Array solution again

--- Heap/Kth Largest Element in an Array/Kth Largest Element in an Array/Program.cs
+++ Heap/Kth Largest Element in an Array/Kth Largest Element in an Array/Program.cs
@@ -58,6 +58,45 @@ class Program
         return heapMin.Peek();
     }
 
+    private static void Swap(int[] nums, int index1, int index2) {
+        int temp = nums[index1];
+        nums[index1] = nums[index2];
+        nums[index2] = temp;
+    }
+
+    private static int QuickSelect(int[] nums, int left, int right, int partitionK) {
+        int swapPointer = left;
+        int pivot = right;
+
+        for (int i = left; i < pivot; i++)
+        {
+            if (nums[i] <= nums[pivot]) {
+                Swap(nums, swapPointer, i);
+                swapPointer++;
+            }
+        }
+
+        Swap(nums, swapPointer, pivot);
+
+        if (swapPointer > partitionK) {
+            return QuickSelect(nums, left, swapPointer - 1, partitionK);
+        }
+        else if (swapPointer < partitionK) {
+            return QuickSelect(nums, swapPointer + 1, right, partitionK);
+        }
+
+        else {
+            return nums[swapPointer];
+        }
+        
+    }
+
+    public static int FindKthLargest3(int[] nums, int k) {
+        int partitionK = nums.Length - k;
+
+        return QuickSelect(nums, 0, nums.Length - 1, partitionK);
+    }
+
     static void Main(string[] args)
     {
         Console.WriteLine(FindKthLargest(TestCase1().nums, TestCase1().k));
@@ -65,5 +104,8 @@ class Program
 
         // Console.WriteLine(FindKthLargest2(TestCase1().nums, TestCase1().k));
         // Console.WriteLine(FindKthLargest2(TestCase2().nums, TestCase2().k));
+
+        // Console.WriteLine(FindKthLargest3(TestCase1().nums, TestCase1().k));
+        // Console.WriteLine(FindKthLargest3(TestCase2().nums, TestCase2().k));
     }
 }
