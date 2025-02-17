c99233e5d638e2805ad71eec6945c76b7459f49c
Huyvictory <vominhhuy008@gmail.com>
2025-02-17 23:36:58 +0700

    Update Contains Duplicate solution

---
+++ Hashing/Contains Duplicate/Contains Duplicate/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace Contains_Duplicate;
+
+class Program
+{
+    private static int[] TestCase1() {
+        return [1, 2, 3, 1];
+    }
+
+    private static int[] TestCase2() {
+        return [1, 2, 3, 4];
+    }
+
+    private static int[] TestCase3() {
+        return [1,1,1,3,3,4,3,2,4,2];
+    }
+
+    private static bool HashMapApproach(int[] nums)
+    {
+        Dictionary<int, int> map = new Dictionary<int, int>();
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (!map.ContainsKey(nums[i]))
+            {
+                map.Add(nums[i], 1);
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public static bool ContainsDuplicate(int[] nums)
+    {
+        return HashMapApproach(nums);
+    }
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine(ContainsDuplicate(TestCase1()));
+        Console.WriteLine(ContainsDuplicate(TestCase2()));
+        Console.WriteLine(ContainsDuplicate(TestCase3()));
+    }
+}
