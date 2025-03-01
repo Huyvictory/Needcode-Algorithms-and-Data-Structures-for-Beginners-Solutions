3f4e14c8306942d8698cb8fd29eab2b0850048e8
Huyvictory <vominhhuy008@gmail.com>
2025-03-01 15:51:12 +0700

    Update House Robber Bottom Up solutions

--- Dynamic Programming/House Robber/House Robber/Program.cs
+++ Dynamic Programming/House Robber/House Robber/Program.cs
@@ -56,9 +56,46 @@ class Program
         return totalMoneyRobbed;
     }
 
+    private int RobBottomUpArrayOptimized(int[] nums) {
+        // First rob attempt get the highest money state of the first house
+        List<int> dpList = new List<int>(2){nums[0], Math.Max(0 + nums[1], nums[0])};
+
+        // Update the maximum money robbed from the next attempt that also depends on two latest robbing attempt
+        // The maximum money is accounted by adding with non adjacent attempt compared with the adjacent one
+        for (int i = 2; i < nums.Length; i++)
+        {
+            int temp = dpList[1];
+            int maxMoneyNextRobAttempt = Math.Max(nums[i] + dpList[0], dpList[1]);
+
+            // Dynamically update the state of two latest rob with largest money each
+            dpList[1] = maxMoneyNextRobAttempt;
+            dpList[0] = temp;
+        }
+
+        return dpList[1];
+    }
+
+    private int RobBottomUpArray(int[] nums) {
+        // First rob attempt get the highest money state of the first house
+        List<int> dpList = new List<int>(){nums[0], Math.Max(0 + nums[1], nums[0])};
+
+        // Update the maximum money robbed from the next attempt that also depends on two latest robbing attempt
+        // The maximum money is accounted by adding with non adjacent attempt compared with the adjacent one
+        for (int i = 2; i < nums.Length; i++)
+        {
+            int maxMoneyNextRobAttempt = Math.Max(nums[i] + dpList[dpList.Count - 2], dpList[dpList.Count - 1]);
+
+            dpList.Add(maxMoneyNextRobAttempt);
+        }
+
+        return dpList[dpList.Count - 1];
+    }
+
     public int Rob(int[] nums)
     {
-        return RobTopDown(nums, 0);
+        if (nums.Length == 1) return nums[0];
+
+        return RobBottomUpArray(nums);
     }
 
     static void Main(string[] args)
