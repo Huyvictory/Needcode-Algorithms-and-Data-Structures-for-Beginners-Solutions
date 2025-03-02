4a6d3ce93d48f6b3497ffd821cd7c27774c45809
Huyvictory <vominhhuy008@gmail.com>
2025-03-02 11:54:33 +0700

    Update Unique Paths Bottom Up solution

--- Dynamic Programming/2-D Dynamic Programming/Unique Paths/Unique Paths/Program.cs
+++ Dynamic Programming/2-D Dynamic Programming/Unique Paths/Unique Paths/Program.cs
@@ -30,6 +30,31 @@ class Program
         return grid[m, n];
     }
 
+    private int UniquePathsBottomUp(int m, int n, int[,] grid)
+    {
+        grid[m - 1, n - 1] = 1;
+
+        // This is for resolving every single sub problem coordinate of the current row
+        int[] recentRowSubProblem = new int[n];
+
+        for (int i = grid.GetUpperBound(0); i >= 0; i--)
+        {
+            grid[i, n - 1] = 1;
+
+            for (int j = n - 2; j >= 0; j--)
+            {
+                // Each sub coordinate will have the result of the same column (go down) + next column (go right)
+                // in the recent row sub problem and current row respectively
+                grid[i, j] = recentRowSubProblem[j] + grid[i, j + 1];
+
+                // Update the recent row sub problem coordinate data to compute for other bigger sub problems
+                recentRowSubProblem[j] = grid[i, j];
+            }
+        }
+
+        return grid[0, 0];
+    }
+
     public int UniquePaths(int m, int n)
     {
         totalRows = m;
@@ -37,7 +62,7 @@ class Program
 
         int[,] grid = new int[m, n];
 
-        return UniquePathsTopDown(0, 0, grid);
+        return UniquePathsBottomUp(totalRows, totalCols, grid);
     }
 
     static void Main(string[] args)
