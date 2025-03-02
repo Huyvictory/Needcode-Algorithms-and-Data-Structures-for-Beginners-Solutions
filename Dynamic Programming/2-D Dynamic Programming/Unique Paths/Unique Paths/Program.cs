5f20c7f242ef9a8b9e5d0e04867e8ae9b9878a49
Huyvictory <vominhhuy008@gmail.com>
2025-03-02 11:21:54 +0700

    Update Unique Paths Top Down solution

---
+++ Dynamic Programming/2-D Dynamic Programming/Unique Paths/Unique Paths/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace Unique_Paths;
+
+class Program
+{
+    private int totalRows = 0;
+    private int totalCols = 0;
+
+    private int UniquePathsTopDown(int m, int n, int[,] grid)
+    {
+        if (m == totalRows || n == totalCols)
+        {
+            return 0;
+        }
+
+        if (m == totalRows - 1 && n == totalCols - 1)
+        {
+            return 1;
+        }
+
+        if (grid[m, n] != 0)
+        {
+            return grid[m, n];
+        }
+
+        int GoDownDecisionUniqueSteps = UniquePathsTopDown(m + 1, n, grid);
+        int GoRightDecisionUniqueSteps = UniquePathsTopDown(m, n + 1, grid);
+
+        grid[m, n] = GoDownDecisionUniqueSteps + GoRightDecisionUniqueSteps;
+
+        return grid[m, n];
+    }
+
+    public int UniquePaths(int m, int n)
+    {
+        totalRows = m;
+        totalCols = n;
+
+        int[,] grid = new int[m, n];
+
+        return UniquePathsTopDown(0, 0, grid);
+    }
+
+    static void Main(string[] args)
+    {
+        Program testProgram = new Program();
+
+        Console.WriteLine(testProgram.UniquePaths(3, 2));
+    }
+}
