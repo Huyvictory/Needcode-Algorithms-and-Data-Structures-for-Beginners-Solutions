471fe2e6a66a0b568c3e23ad98c1208141e62bc2
Huyvictory <vominhhuy008@gmail.com>
2025-03-03 15:14:28 +0700

    Update Number of 1 Bits solution

---
+++ Bit Manipulation/Number of 1 Bits/Number of 1 Bits/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Number_of_1_Bits;
+
+class Program
+{
+    public int HammingWeight(int n)
+    {
+        int count = 0;
+
+        while (n > 0)
+        {
+            // If the current bit bitwise and with 1 as (0000 0000 0000 0001)
+            // give out a valid 1 bit then count up
+            if ((n & 1) == 1)
+            {
+                count++;
+            }
+
+            // Shift bits of the integer n to the right and check for 1 bit at the end
+            n = n >> 1;
+        }
+
+        return count;
+    }
+
+    static void Main(string[] args)
+    {
+        Program testProgram = new Program();
+
+        Console.WriteLine(testProgram.HammingWeight(2147483645));
+    }
+}
