e7036d0ea961ae845e90bb20ff20c3ea4998a0f6
Huyvictory <vominhhuy008@gmail.com>
2025-03-03 16:05:24 +0700

    Update Counting Bits solution

---
+++ Bit Manipulation/Counting Bits/Counting Bits/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace Counting_Bits;
+
+class Program
+{
+    private int BitCounting(int n)
+    {
+        int count = 0;
+
+        while (n > 0)
+        {
+            // Get rid of the 1 bit in the n integer
+            n &= n - 1;
+
+            count++;
+        }
+
+        return count;
+    }
+
+    public int[] CountBits(int n)
+    {
+        int[] ans = new int[n + 1];
+
+        for (int i = 0; i < ans.Length; i++)
+        {
+            ans[i] = BitCounting(i);
+        }
+
+        return ans;
+    }
+
+    static void Main(string[] args)
+    {
+        Program testProgram = new Program();
+
+        testProgram.CountBits(5);
+
+        return;
+    }
+}
