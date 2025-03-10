44ccd50e1cbc4a6e6e1916108c67a40f37ea5ea5
Huyvictory <vominhhuy008@gmail.com>
2025-03-10 14:02:28 +0700

    Update Valid Palindrome solution

---
+++ Arrays/Two Pointers/Valid Palindrome/Valid Palindrome/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace Valid_Palindrome;
+
+class Program
+{
+    private static string TestCase1()
+    {
+        return "A man, a plan, a canal: Panama";
+    }
+
+    private static string TestCase2()
+    {
+        return "race a car";
+    }
+
+    private static string TestCase3()
+    {
+        return " ";
+    }
+
+    public static bool IsPalindromeTwoPointers(string s)
+    {
+        s = new string(s.ToLower().Where(c => char.IsLetterOrDigit(c)).ToArray());
+
+        int left = 0;
+        int right = s.Length - 1;
+
+        while (left < right)
+        {
+            if (s[left] != s[right])
+            {
+                return false;
+            }
+
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+
+    public static bool IsPalindrome(string s)
+    {
+        return IsPalindromeTwoPointers(s);
+    }
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine(IsPalindromeTwoPointers(TestCase1()));
+    }
+}
