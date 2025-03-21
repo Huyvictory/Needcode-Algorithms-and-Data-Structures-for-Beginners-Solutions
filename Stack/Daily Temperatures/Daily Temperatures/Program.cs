b8f21977d05442cbf4bf93c59bdf0f482b009c62
Huyvictory <vominhhuy008@gmail.com>
2025-03-21 14:11:03 +0700

    Update Daily Temperatures Brute force solution

---
+++ Stack/Daily Temperatures/Daily Temperatures/Program.cs
@@ -0,0 +1,53 @@
+﻿namespace Daily_Temperatures;
+
+class Program
+{
+    private static int[] TestCase1() {
+        return [73,74,75,71,69,72,76,73];
+    }
+
+    private static int[] TestCase2() {
+        return [30,40,50,60];
+    }
+
+    private static int[] TestCase3() {
+        return [30,60,90];
+    }
+
+    // TC: O(n^2), SC: O(n)
+    private static int[] DailyTemperaturesBruteForce(int[] temperatures)
+    {
+        int[] answer = new int[temperatures.Length];
+
+        for (int i = 0; i < temperatures.Length; i++)
+        {
+            int numberOfWaitingDays = 1;
+
+            for (int j = i + 1; j < temperatures.Length; j++)
+            {
+                if (temperatures[j] > temperatures[i])
+                {
+                    answer[i] = numberOfWaitingDays;
+                    break;
+                }
+                else {
+                    numberOfWaitingDays++;
+                }
+            }
+        }
+
+        return answer;
+    }
+
+    public static int[] DailyTemperatures(int[] temperatures)
+    {
+        return DailyTemperaturesBruteForce(temperatures);
+    }
+
+    static void Main(string[] args)
+    {
+        var result = DailyTemperatures(TestCase1());
+
+        return;
+    }
+}
