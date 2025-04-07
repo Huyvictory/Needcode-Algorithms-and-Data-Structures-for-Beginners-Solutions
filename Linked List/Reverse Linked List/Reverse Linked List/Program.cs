d5bced1eecf7ea6a6304f21c5ee2c890700f1420
Huyvictory <vominhhuy008@gmail.com>
2025-04-07 12:53:44 +0700

    Update Reverse Linked List Two Pointers solution

--- Linked List/Reverse Linked List/Reverse Linked List/Program.cs
+++ Linked List/Reverse Linked List/Reverse Linked List/Program.cs
@@ -2,26 +2,18 @@
 
 class Program
 {
-    public static ListNode ListNode5 = new ListNode(5);
-    public static ListNode ListNode4 = new ListNode(4, ListNode5);
-    public static ListNode ListNode3 = new ListNode(3, ListNode4);
-    public static ListNode ListNode2 = new ListNode(2, ListNode3);
-    public static ListNode ListNode1 = new ListNode(1, ListNode2);
-    public static List<ListNode> testCase1 = new List<ListNode>
+    private static ListNode TestCase1()
     {
-        ListNode1,
-        ListNode2,
-        ListNode3,
-        ListNode4,
-        ListNode5
-    };
+        var ListNode5 = new ListNode(5);
+        var ListNode4 = new ListNode(4, ListNode5);
+        var ListNode3 = new ListNode(3, ListNode4);
+        var ListNode2 = new ListNode(2, ListNode3);
+        var head = new ListNode(1, ListNode2);
 
-    static void Main(string[] args)
-    {
-        Console.WriteLine(ReverseList(ListNode1).val);
+        return head;
     }
 
-    public static ListNode ReverseList(ListNode head)
+    private static ListNode ReverseListThreePointers(ListNode head)
     {
         if (head == null)
             return null;
@@ -42,5 +34,50 @@ class Program
         }
 
         return previousNode;
+    }
+
+    // TC: O(n), SC: O(1)
+    private static ListNode ReverseListTwoPointers(ListNode head)
+    {
+        if (head == null)
+            return null;
+
+        // Pointer cur that traverse on original linked list
+        var cur = head.next;
+
+        // Pointer newHead to keep track all of the swaps reverse
+        // By default this is the head of original linked list but has been changed to the tail node of reversed linked list
+        var newHead = head;
+        newHead.next = null;
+
+        while (cur != null)
+        {
+            // Keep track of the next node of original linked list
+            var nextNode = cur.next;
+
+            // Reverse the pointer reference of every node in original linked list
+            cur.next = newHead;
+
+            // Move pointer newHead to next new head position
+            newHead = cur;
+
+            // Move to next node of original linked list for next swap reverse
+            cur = nextNode;
+        }
+
+        return newHead;
+    }
+
+    public static ListNode ReverseList(ListNode head)
+    {
+        // return ReverseListThreePointers(head);
+        return ReverseListTwoPointers(head);
+    }
+
+    static void Main(string[] args)
+    {
+        var result = ReverseList(TestCase1());
+
+        return;
     }
 }
