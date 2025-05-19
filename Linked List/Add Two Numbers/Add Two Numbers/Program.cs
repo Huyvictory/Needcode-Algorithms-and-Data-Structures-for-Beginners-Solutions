356fa7d0a57ef7226fc288c357de85e4c7758eb8
Huyvictory <vominhhuy008@gmail.com>
2025-05-19 18:30:28 +0700

    Update Three Pointers shorted solution

--- Linked List/Add Two Numbers/Add Two Numbers/Program.cs
+++ Linked List/Add Two Numbers/Add Two Numbers/Program.cs
@@ -142,14 +142,48 @@ class Program
         return result;
     }
 
+    // TC: O(m + n), SC: O(1)
+    private static ListNode AddTwoNumbersThreePointersShortened(ListNode l1, ListNode l2)
+    {
+        var pointerL1 = l1;
+        var pointerL2 = l2;
+
+        ListNode dummyNode = new ListNode(0);
+        var pointerResult = dummyNode;
+        var carry = 0;
+
+        while (pointerL1 != null || pointerL2 != null || carry != 0)
+        {
+            ListNode newNode = new ListNode(0);
+
+            var l1Value = pointerL1 != null ? pointerL1.val : 0;
+            var l2Value = pointerL2 != null ? pointerL2.val : 0;
+
+            var valueToUpdate = (l1Value + l2Value + carry) % 10;
+            carry = (l1Value + l2Value + carry) / 10;
+
+            newNode.val = valueToUpdate;
+
+            pointerResult.next = newNode;
+
+            pointerResult = newNode;
+
+            pointerL1 = pointerL1 != null && pointerL1.next != null ? pointerL1.next : null;
+            pointerL2 = pointerL2 != null && pointerL2.next != null ? pointerL2.next : null;
+        }
+
+        return dummyNode.next;
+    }
+
     public static ListNode AddTwoNumbers(ListNode l1, ListNode l2)
     {
-        return AddTwoNumbersThreePointers(l1, l2);
+        // return AddTwoNumbersThreePointers(l1, l2);
+        return AddTwoNumbersThreePointersShortened(l1, l2);
     }
 
     static void Main(string[] args)
     {
-        var result = AddTwoNumbers(TestCase3().l1, TestCase3().l2);
+        var result = AddTwoNumbers(TestCase1().l1, TestCase1().l2);
 
         return;
     }
